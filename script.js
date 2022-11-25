b8b0d99ba9873eb6a8ddc5a2d6c4bd89b6790997
j-haiki <108611758+j-haiki@users.noreply.github.com>
2022-11-26 01:21:33 +0300

    added UI to the game

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
 // let player choose an option
 // compare between computer choice and player choice
 // repeat 5 times
+//--------
 
-function computer() {
+function computerChoice() {
   let computerRNG = Math.floor(Math.random() * 2.99999999999);
   if (computerRNG == 0) {
     return "rock";
@@ -15,70 +16,50 @@ function computer() {
   }
 }
 
-function player() {
-  choice = prompt("make a choice!").toLowerCase();
-  alert(`You chose ${choice}`);
-  return choice;
-}
-
 let playerScore = 0;
 let computerScore = 0;
 let drawScore = 0;
 
 function playRound(player, computer) {
+  console.log("pog");
   if (player == computer) {
     drawScore++;
-    return "it's a tie";
+    //("it's a tie");
   } else if (player == "rock" && computer == "scissors") {
     playerScore++;
-    return "You Win! Rock beats Scissors";
+    //alert("You Win! Rock beats Scissors");
   } else if (player == "rock" && computer == "paper") {
     computerScore++;
-    return "You Lose! Paper beats Rock";
+    //alert("You Lose! Paper beats Rock");
   } else if (player == "paper" && computer == "rock") {
     playerScore++;
-    return "You Win! Paper beats Rock";
+    //alert("You Win! Paper beats Rock");
   } else if (player == "paper" && computer == "scissors") {
     computerScore++;
-    return "You Lose! Scissors beats Paper";
+    //alert("You Lose! Scissors beats Paper");
   } else if (player == "scissors" && computer == "paper") {
     playerScore++;
-    return "You Win! Scissors beats Paper";
+    //alert("You Win! Scissors beats Paper");
   } else if (player == "scissors" && computer == "rock") {
     computerScore++;
-    return "You Lose! Rock beats Scissors";
-  } else if (player == "scissor" && computer == "paper") {
-    playerScore++;
-    return "You Win! Scissors beats Paper";
-  } else if (player == "scissor" && computer == "rock") {
-    computerScore++;
-    return "You Lose! Rock beats Scissors";
-  } else {
-    computerScore++;
-    return "You Lose! Please enter a valid choice next time"; // this is intentionally mean
+    //alert("You Lose! Rock beats Scissors");
   }
+  youChose = document.getElementById("selection");
+  youChose.innerText = `You Chose ${player} Computer Chose ${computer}`;
+
+  resultUpdate = document.getElementById("results");
+  resultUpdate.innerText = `Player: ${playerScore}⠀⠀⠀⠀Computer: ${computerScore}⠀⠀⠀⠀Draws: ${drawScore}`;
+
+  if (playerScore == 5) {
+    playerScore = 0;
+    computerScore = 0;
+    drawScore = 0;
+    youChose.innerText = `You Win!!`;
+  } else if (computerScore == 5) {
+    playerScore = 0;
+    computerScore = 0;
+    drawScore = 0;
+    youChose.innerText = `You Lose :(`;
+  }
+  //return resultUpdate;
 }
-
-function game() {
-  for (let i = 1; i <= 5; i++) {
-    console.log(
-      `ROUND: ${i} ${playRound(
-        player(),
-        computer()
-      )} player: ${playerScore} Computer: ${computerScore}`
-    );
-  }
-
-  if (playerScore > computerScore) {
-    console.log("You Win!");
-  } else if (computerScore > playerScore) {
-    console.log("You Lose!");
-  } else {
-    console.log("its a Draw!");
-  }
-  console.log(
-    `Final scores: You:${playerScore} Computer:${computerScore} Draws:${drawScore}`
-  );
-}
-
-game();
